ed924bb832b91accba48d89c724adf39a4686b38
zdtz <dduythien.it@gmail.com>
2024-05-21 23:14:27 +0700

    update theme

--- wp-content/uploads/elementor-widget/class-pxl-icon-hidden-panel.php
+++ wp-content/uploads/elementor-widget/class-pxl-icon-hidden-panel.php
@@ -5,7 +5,7 @@ class PxlIconHiddenPanel_Widget extends Pxltheme_Core_Widget_Base{
     protected $title = 'Hidden Panel Pxl';
     protected $icon = 'eicon-menu-bar';
     protected $categories = array( 'pxltheme-core' );
-    protected $params = '{"sections":[{"name":"section_content","label":"Content","tab":"content","controls":[{"name":"content_template","label":"Select Template","type":"select","options":{"0":"None","3871":"Hidden Sidebar"},"default":"df","description":"Add new tab template: \"<a href=\"https:\/\/demo.bravisthemes.com\/digicove\/wp-admin\/edit.php?post_type=pxl-template\" target=\"_blank\">Click Here<\/a>\""},{"name":"icon_color","label":"Icon Color","type":"color","selectors":{"{{WRAPPER}} .pxl-hidden-panel-button .pxl-icon-line, {{WRAPPER}} .pxl-hidden-panel-button .pxl-icon-line":"background-color: {{VALUE}};"}}]}]}';
+    protected $params = '{"sections":[{"name":"section_content","label":"Content","tab":"content","controls":[{"name":"content_template","label":"Select Template","type":"select","options":["None"],"default":"df","description":"Add new tab template: \"<a href=\"http:\/\/localhost\/manhinhaptech\/wp-admin\/edit.php?post_type=pxl-template\" target=\"_blank\">Click Here<\/a>\""},{"name":"icon_color","label":"Icon Color","type":"color","selectors":{"{{WRAPPER}} .pxl-hidden-panel-button .pxl-icon-line, {{WRAPPER}} .pxl-hidden-panel-button .pxl-icon-line":"background-color: {{VALUE}};"}}]}]}';
     protected $styles = array(  );
     protected $scripts = array(  );
 }
